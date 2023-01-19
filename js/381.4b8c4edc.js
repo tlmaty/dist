1f83c43fc37d66aace81ba484e9dd62c2ad0d561
tlmaty <t.lambolez@maty.fr>
2023-01-19 13:38:46 +0100

    deploy

---
+++ js/381.4b8c4edc.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktest_vue"]=self["webpackChunktest_vue"]||[]).push([[381],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return l}});var u=n(3396);const s={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),r=[a];function c(t,e){return(0,u.wg)(),(0,u.iD)("div",s,r)}var i=n(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=381.4b8c4edc.js.map
